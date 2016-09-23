0a5a7de568c30f4f49a0e4ed2368e9852ed01008
Joana Modesto Hrotko <ist181423@sigma01.ist.utl.pt>
2016-09-23 23:53:35 +0100

    TRS list working.

--- TCS/include/queue.h
+++ TCS/include/queue.h
@@ -1,42 +1,40 @@
-#ifndef QUEUE
-#define QUEUE 
+#ifndef __LIST__
+#define __LIST__
 
-#include <stdio.h>
-#include <stdlib.h>
-#include "q_item.h"
+typedef struct q_node* node_link;
+typedef struct trsItem* trs_item;
+typedef struct trsList* trs_list;
 
-typedef struct q_node* q_link;
+struct trsItem
+{
+    char* language;
+    char* hostname;
+    unsigned int port;
+};
 
-struct q_node { 
-        Q_item item; 
-        q_link next; 
-};  								
+struct q_node
+{
+    trs_item item;
+    node_link next;
+};
 
-typedef struct queue { 
-     q_link head; 						
-     q_link tail; 					
-     int size; 						  
+struct trsList
+{
+    node_link head;
+    int size;
 
-} *Queue; 
-
-/*
- * Estrutura FIFO (embora seja possivel remover qualquer elemento).
- * A destruicao dos items retirados da Queue e da responsabilidade
- * do utilizador, e estes nao sao apagados quando a Queue e apagada.
- * 
- */
-									
+};
 
 
-q_link create_q_node(Q_item i);
-void destroy_q_node(q_link n);
-Queue createQueue();
-void q_push(Queue q, Q_item i);
-Q_item q_pop(Queue q); 
-Q_item q_find(Queue q, Q_KEY k); 
-Q_item q_remove(Queue q, Q_KEY k); 
-void q_show(Queue q);
-void deleteQueue(Queue q);
-
+trs_item createTRS(char* language, char* hostname, unsigned int port);
+void destroyTRS(trs_item trs);
+node_link createNode(trs_item trs);
+void destroyNode(node_link node);
+trs_list createList();
+void addTRS(trs_list list, trs_item trs);
+void removeTRS(trs_list list, char* language);
+int sizeList(trs_list list);
+void destroyList(trs_list list);
+void showList(trs_list list);
 
 #endif
