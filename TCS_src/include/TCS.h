36630bc01eafc5d6729cbf29e31e4463d29b0e8d
Joana Modesto Hrotko <ist181423@lab10p2.rnl.tecnico.ulisboa.pt>
2016-10-13 16:58:46 +0100

    It is even prettier than before

--- TCS_src/include/TCS.h
+++ TCS_src/include/TCS.h
@@ -5,10 +5,21 @@
 
 #define MAX 1024
 
+/* Gets language from buffer. language2 will have the result and th errors 
+to be sent*/
 void getBufferLanguage(char buffer[], char language2[]);
+
+/*Receives the language requested by the serverFD and finds the TRS server*/
 void getTRSInfo(trs_list list, char* language, char repply[]);
+
+/*Verefies if a trs exists with a language requested on the buffer.*/
 void checkTRS(trs_list list, char buffer[], char repply[]);
+
+/* When the trs stops translating a language, ths tcs will verify if it is in the trs_list
+and if it is in fact it removes from the list. */
 void stopTranslating(trs_list list, char buffer[], char repply[]);
+
+/*Finishes the program properly.*/
 void finishProgram(int* user, trs_list list, const char* error);
 
 #endif
