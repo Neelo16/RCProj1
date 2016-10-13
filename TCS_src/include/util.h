a60a4c55a19748ab8bd73f073a468ddf7331f783
Neelo <neelo_sim@hotmail.com>
2016-10-13 18:12:59 +0100

    Changed TCS util.c and .h to symbolic links

--- TCS_src/include/util.h
+++ TCS_src/include/util.h
@@ -1,10 +1,1 @@
-#ifndef UTIL_H
-#define UTIL_H
-
-#define GROUPNUMBER 15
-#define PORT 58000+GROUPNUMBER
-
-void exitMsg(const char *msg);
-void *safeMalloc(size_t size);
-
-#endif
+../../user_src/include/util.h
